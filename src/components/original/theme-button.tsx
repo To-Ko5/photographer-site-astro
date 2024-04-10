de3baf859218c88d83a806e01b2425fe77b5435a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-10 09:56:36 +0900

    feat(darkmode): theme-button add

---
+++ src/components/original/theme-button.tsx
@@ -0,0 +1,39 @@
+import { MoonIcon, SunIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
+
+const ThemeButton = () => {
+  const [theme, setThemeState] = useState<'theme-light' | 'dark' | 'system'>(
+    'theme-light'
+  )
+
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains('dark')
+    setThemeState(isDarkMode ? 'dark' : 'theme-light')
+  }, [])
+
+  useEffect(() => {
+    const isDark =
+      theme === 'dark' ||
+      (theme === 'system' &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
+  }, [theme])
+
+  const handleClick = () => {
+    if (theme === 'dark') {
+      setThemeState('theme-light')
+    } else {
+      setThemeState('dark')
+    }
+  }
+  return (
+    <Button variant="outline" size="icon" onClick={handleClick}>
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
+
+export default ThemeButton
