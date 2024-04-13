c7557d80489d9f22d71d4f3eedb7fcdcf77e912f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-13 09:48:34 +0900

    feat(style): masonry css add

--- src/components/original/photos-mesonry.tsx
+++ src/components/original/photos-mesonry.tsx
@@ -2,38 +2,50 @@ import React from 'react'
 
 const PhotosMasonry = () => {
   return (
-    <div>
-      <div>
+    <div className="columns-2 md:columns-3 gap-4">
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo1.jpg" />
           <img src="/photos/photo1.jpg" alt="icon" className="" />
         </picture>
       </div>
-      <div>
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo2.jpg" />
           <img src="/photos/photo2.jpg" alt="icon" className="" />
         </picture>
       </div>
-      <div>
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo3.jpg" />
           <img src="/photos/photo3.jpg" alt="icon" className="" />
         </picture>
       </div>
-      <div>
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo2.jpg" />
           <img src="/photos/photo2.jpg" alt="icon" className="" />
         </picture>
       </div>
-      <div>
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo2.jpg" />
           <img src="/photos/photo2.jpg" alt="icon" className="" />
         </picture>
       </div>
-      <div>
+      <div className="mb-4">
+        <picture>
+          <source srcSet="/photos/photo1.jpg" />
+          <img src="/photos/photo1.jpg" alt="icon" className="" />
+        </picture>
+      </div>
+      <div className="mb-4">
+        <picture>
+          <source srcSet="/photos/photo1.jpg" />
+          <img src="/photos/photo1.jpg" alt="icon" className="" />
+        </picture>
+      </div>
+      <div className="mb-4">
         <picture>
           <source srcSet="/photos/photo1.jpg" />
           <img src="/photos/photo1.jpg" alt="icon" className="" />
