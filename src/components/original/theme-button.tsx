d7b2d4e728a0e00a015cab97c236830fc8339a19
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-18 00:00:53 +0900

    feat(theme): style add

--- src/components/original/theme-button.tsx
+++ src/components/original/theme-button.tsx
@@ -28,9 +28,14 @@ const ThemeButton = () => {
     }
   }
   return (
-    <Button variant="outline" size="icon" onClick={handleClick}>
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={handleClick}
+      className="bg-transparent hover:bg-gray-100/5"
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] text-muted rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] text-muted rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
