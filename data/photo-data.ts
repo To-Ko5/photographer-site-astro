b872866a7f4645612fbe0bef95cfd939aad80295
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-17 14:12:22 +0900

    feat(data): dummy data add

---
+++ data/photo-data.ts
@@ -0,0 +1,69 @@
+import photo1 from '../public/photos/photo1.jpg'
+import photo2 from '../public/photos/photo2.jpg'
+import photo3 from '../public/photos/photo3.jpg'
+
+export const photos = [
+  {
+    src: photo1,
+    title: 'title1',
+    description: 'title1の画像',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo2,
+    title: 'title2',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo3,
+    title: 'title3',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo2,
+    title: 'title4',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo1,
+    title: 'title5',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo2,
+    title: 'title6',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo3,
+    title: 'title7',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo2,
+    title: 'title8',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  },
+  {
+    src: photo1,
+    title: 'title9',
+    description: 'test',
+    date: '2024-01-24',
+    link: 'http://localhost:4321/url'
+  }
+]
