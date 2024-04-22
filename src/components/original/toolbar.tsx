d25dbeb8ba3b3fe085891e7c2d4546a6c70c0c19
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-22 11:20:26 +0900

    feat(toolbar): avatar add

--- src/components/original/toolbar.tsx
+++ src/components/original/toolbar.tsx
@@ -1,14 +1,15 @@
 import { Separator } from '@/components/ui/separator'
 import ThemeButton from './theme-button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 const toolbar = () => {
   return (
     <div className="fixed flex items-center w-max shadow-lg backdrop-blur p-2 bottom-8 left-1/2 -translate-x-1/2 rounded-md bg-muted/10 border">
       <div>
-        <picture>
-          <source srcSet="/icon.jpg" />
-          <img src="/icon.jpg" alt="icon" className="max-w-8" />
-        </picture>
+        <Avatar className="rounded-md">
+          <AvatarImage src="/icon.jpg" alt="仮" />
+          <AvatarFallback>仮</AvatarFallback>
+        </Avatar>
       </div>
       <Separator orientation="vertical" className="h-8 mx-2" />
       <ThemeButton />
