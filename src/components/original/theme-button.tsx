23126c9ce61b7f7707040113d0beb1a219936da7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-23 11:11:42 +0900

    refactor(theme): style edited

--- src/components/original/theme-button.tsx
+++ src/components/original/theme-button.tsx
@@ -32,10 +32,10 @@ const ThemeButton = () => {
       variant="outline"
       size="icon"
       onClick={handleClick}
-      className="bg-transparent hover:bg-gray-100/5"
+      className="bg-transparent hover:bg-gray-100/5 border-muted-foreground"
     >
-      <SunIcon className="h-[1.2rem] w-[1.2rem] text-muted rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] text-muted rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
